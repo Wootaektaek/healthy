2a63c83ee317e279e256ea83a8d3d370b23592ac
Wootaektaek <93571429+Wootaektaek@users.noreply.github.com>
2022-10-28 15:04:35 +0900

    Add files via upload

---
+++ heathy.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import webbrowser
+from PIL import Image
+st.title("사용자 맞춤 운동 추천 서비스 헬띠")
+
+st.write("안녕하세요. 헬띠입니다. 설문을 통해 사용자에게 운동을 추천하고 사진을 업로드하면 BMI를 알려줍니다. 이를 통해 하루하루 달라지는 본인을 확인하세요.")
+
+user_name = st.text_input("사용자의 이름을 입력하세요: ")
+
+user_face = st.camera_input('오늘의 당신을 알려주세요.')
