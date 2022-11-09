e106a25f51f290942d2209c991db0e73a3de677c
TaeKwan Lee <87294861+taekwanlee@users.noreply.github.com>
2022-11-09 16:00:16 +0900

    Update heathy.py

--- heathy.py
+++ heathy.py
@@ -69,11 +69,9 @@ ex_image = Image.open('leedaeho.jpeg')
 
 st.image(ex_image, caption='이처럼 정면을 바라본 이미지를 업로드 해주세요')
 
-# user_face = st.camera_input('오늘의 당신을 알려주세요.')
-
 # ==================================================================================
 # 아래부터 face2bmi 이미지 출력 코드
-import cv2
+
 st.subheader('')
 st.subheader("얼굴 이미지로 예측해보는 BMI")
 
