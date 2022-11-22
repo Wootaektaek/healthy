130b104ff7d6f464e36bbefd7b5f5d5a2032e588
TaeKwan Lee <87294861+taekwanlee@users.noreply.github.com>
2022-11-22 22:07:50 +0900

    Add files via upload

---
+++ pages/3_💕_Signup.py
@@ -0,0 +1,15 @@
+
+import streamlit as st
+import streamlit_authenticator as stauth
+import database_
+
+
+st.subheader("새 계정을 만듭니다.")
+name = st.text_input('이름을 입력해주세요.')
+new_user = st.text_input("아이디를 입력해주세요.")
+new_password = st.text_input("패스워드를 입력해주세요.",type='password')
+
+if st.button("회원가입"):
+  database_.insert_user(new_user, name, new_password)
+  st.success("회원가입이 완료되었습니다!")
+  st.info("로그인 화면에서 로그인 해주세요.")
